0b6c9e02e846b766c91198f26dcff9872798e248
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2022-10-26 16:44:26 +0200

    Added backup function

--- SDP/BthPortDevice.cs
+++ SDP/BthPortDevice.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using System.Net.NetworkInformation;
+
 using JetBrains.Annotations;
+
 using Microsoft.Win32;
 
 namespace Nefarius.Utilities.Bluetooth.SDP;
@@ -23,7 +25,48 @@ public sealed class BthPortDevice
     public byte[] CachedServices
     {
         get => (byte[])_cachedServicesKey?.GetValue("00010001");
-        set => _cachedServicesKey.SetValue("00010001", value, RegistryValueKind.Binary);
+        set
+        {
+            // make backup copy
+            _cachedServicesKey.SetValue("Nefarius-00010001-Backup", CachedServices, RegistryValueKind.Binary);
+            // update original value
+            _cachedServicesKey.SetValue("00010001", value, RegistryValueKind.Binary);
+        }
+    }
+
+    [UsedImplicitly]
+    public bool IsCachedServicesPatched
+    {
+        get
+        {
+            byte[] content = (byte[])_cachedServicesKey?.GetValue("Nefarius-00010001-Backup");
+
+            return content is not null;
+        }
+    }
+
+    [UsedImplicitly]
+    public byte[] OriginalCachedServices
+    {
+        get
+        {
+            if (!IsCachedServicesPatched)
+            {
+                return CachedServices;
+            }
+
+            return (byte[])_cachedServicesKey?.GetValue("Nefarius-00010001-Backup");
+        }
+    }
+
+    public void DeleteOriginalCachedServices()
+    {
+        if (!IsCachedServicesPatched)
+        {
+            return;
+        }
+
+        _cachedServicesKey?.DeleteValue("Nefarius-00010001-Backup");
     }
 
     public override string ToString()
