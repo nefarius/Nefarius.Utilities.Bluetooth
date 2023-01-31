98724af324d2e699f5c14472f5823c1b0c3f8e92
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-01-31 12:33:54 +0100

    Added new types

---
+++ Types/BTH_SDP_CONNECT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Nefarius.Utilities.Bluetooth.Types;
+
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+internal struct BTH_SDP_CONNECT
+{
+#pragma warning disable CS0169
+#pragma warning disable IDE1006
+    internal UInt64 bthAddress;
+    internal UInt32 fSdpConnect;
+    internal UInt64 hConnection;
+    internal byte requestTimeout;
+#pragma warning restore CS0169
+#pragma warning restore IDE1006
+}
---
+++ Types/BTH_SDP_DISCONNECT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Nefarius.Utilities.Bluetooth.Types;
+
+[StructLayout(LayoutKind.Sequential)]
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Local")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+[SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+internal struct BTH_SDP_DISCONNECT
+{
+#pragma warning disable CS0169
+#pragma warning disable IDE1006
+    internal UInt64 hConnection;
+#pragma warning restore CS0169
+#pragma warning restore IDE1006
+}
